4b2be0874566f75c3a74f147ceba9402f3683480
Benji Sidi <benjisidi@gmail.com>
2020-09-21 18:27:30 +0100

    Implemented a dictionary for widths to prevent argument expolsion as more parameters are added

--- generate-clocks.py
+++ generate-clocks.py
@@ -19,7 +19,13 @@ def get_hour_hand_angle(time):
 
 
 class Clock:
-    def __init__(self, time, clock_width, hour_width=1, minute_width=1):
+    def __init__(self, time, widths=None):
+        # Initialize pixel widths with default values
+        if widths is None:
+            widths = {}
+        widths = {**{"clock": 5, "minute": 2, "hour": 2}, **widths}
+
+        # Create empty image and calculate content
         img = Image.new(mode="L", size=(100, 100))
         minute_hand_angle = time[1] * 6
         minute_hand_x = 50 + 40 * math.sin(math.radians(minute_hand_angle))
@@ -27,12 +33,16 @@ class Clock:
         hour_hand_angle = time[0] * 30 + time[1] * 0.5
         hour_hand_x = 50 + 20 * math.sin(math.radians(hour_hand_angle))
         hour_hand_y = 50 + 20 * math.cos(math.radians(hour_hand_angle + 180))
+
+        # Draw clock components
         draw = ImageDraw.Draw(img)
-        draw.arc([10, 10, 90, 90], 0, 360, "white", width=width)
+        draw.arc([10, 10, 90, 90], 0, 360, "white", width=widths["clock"])
         draw.line(
-            [50, 50, minute_hand_x, minute_hand_y], width=minute_width, fill="white"
+            [50, 50, minute_hand_x, minute_hand_y], width=widths["minute"], fill="white"
         )
-        draw.line([50, 50, hour_hand_x, hour_hand_y], width=hour_width, fill="white")
+        draw.line(
+            [50, 50, hour_hand_x, hour_hand_y], width=widths["hour"], fill="white"
+        )
         self.img = img
 
     def show(self):
@@ -40,8 +50,8 @@ class Clock:
         plt.show()
 
 
-clock1 = Clock([5, 30], 5)
-clock2 = Clock([3, 15], 3)
+clock1 = Clock([5, 30], {"clock": 1})
+clock2 = Clock([3, 15], {"hour": 3, "minute": 2, "clock": 4})
 
 clock1.show()
 clock2.show()
